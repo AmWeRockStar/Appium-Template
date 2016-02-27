0d47f01e38c7734ed742fef7f3dcd9e542b06e9a
WeRockStar <vr_freestyle@outlook.com>
2016-02-27 20:22:11 +0700

    Refactor test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -12,19 +12,29 @@ public class SampleTest {
     @BeforeClass
     public static void setUp() throws Exception {
         BaseSettings settings = new BaseSettings();
-        driver =  settings.getDriver();
+        driver = settings.getDriver();
     }
 
     @Test
-    public void clickOnButton() throws Exception{
+    public void when_click_on_button_should_see_dialog_message_hello_click() throws Exception {
         WebElement btnSample = driver.findElement(By.id("com.geekybase.appfortraining:id/button"));
         btnSample.click();
+        WebElement message = driver.findElement(By.id("android:id/message"));
+        assertEquals("Hello click", message.getText());
     }
+
     @Test
-    public void getTextFromTextView() throws Exception {
+    public void should_see_text_sample_text() throws Exception {
         WebElement tvSomeText = driver.findElement(By.id("com.geekybase.appfortraining:id/textView"));
         String expected = "Sample Text";
         assertEquals(expected, tvSomeText.getText());
+    }
+
+    @Test
+    public void when_click_toggle_button_should_see_close_state_to_open_state() throws Exception {
+        WebElement toggleBtn = driver.findElement(By.id("com.geekybase.appfortraining:id/toggleButton"));
+        toggleBtn.click();
+        assertEquals("เปิด", toggleBtn.getText());
     }
 
 
