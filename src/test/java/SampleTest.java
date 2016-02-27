4068ccdbcf9fc263f7e0544986f5d67508781912
WeRockStar <vr_freestyle@outlook.com>
2016-02-27 22:36:42 +0700

    Implement test and rename test case

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,17 @@
 import core.BaseSettings;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.*;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.TestCase.assertEquals;
 
+@FixMethodOrder(MethodSorters.JVM)
 public class SampleTest {
     static AndroidDriver driver;
 
@@ -21,6 +27,7 @@ public class SampleTest {
         btnSample.click();
         WebElement message = driver.findElement(By.id("android:id/message"));
         assertEquals("Hello click", message.getText());
+        (driver.findElement(By.name("OK"))).click();
     }
 
     @Test
@@ -37,6 +44,49 @@ public class SampleTest {
         assertEquals("เปิด", toggleBtn.getText());
     }
 
+    @Test
+    public void when_press_on_checkbox_state_should_are_change() throws Exception {
+        WebElement chkBox = driver.findElement(By.id("com.geekybase.appfortraining:id/checkBox"));
+        chkBox.click();
+        assertEquals("" + true, chkBox.getAttribute("checked"));
+    }
+
+    @Test
+    public void move_seekbar_from_0_to_something() throws Exception {
+        WebElement seekBar = driver.findElement(By.id("com.geekybase.appfortraining:id/seekBar"));
+        int originalX = seekBar.getLocation().getX();
+        int originY = seekBar.getLocation().getY();
+
+        int moveTo = originalX + seekBar.getSize().getWidth();
+        TouchAction tAcion = new TouchAction(driver);
+        tAcion.press(originalX, originY).moveTo(moveTo - 10, originY).release().perform();
+    }
+
+    @Test
+    public void put_github_username_should_see_fullname() throws Exception {
+        driver.startActivity("com.geekybase.appfortraining", "com.geekybase.appfortraining.activity.ThreeActivity");
+
+        WebElement edtUsername = driver.findElement(By.id("com.geekybase.appfortraining:id/edtUser"));
+        edtUsername.sendKeys("werockstar");
+        WebElement btnSearch = driver.findElement(By.id("com.geekybase.appfortraining:id/btnSearch"));
+        btnSearch.click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.geekybase.appfortraining:id/tvFullName")));
+
+        WebElement textFullName = driver.findElement(By.id("com.geekybase.appfortraining:id/tvFullName"));
+        assertEquals("Kotchaphan Muangsan", textFullName.getText());
+    }
+
+    @Test
+    public void swipe_from_one_fragment_to_two_fragment() throws Exception {
+        driver.startActivity("com.geekybase.appfortraining", "com.geekybase.appfortraining.activity.FourActivity");
+        Thread.sleep(400);
+//        int w = driver.manage().window().getSize().getWidth();
+//        int h = driver.manage().window().getSize().getHeight();
+        driver.swipe(800, 500, 100, 500, 400);
+        WebElement content = driver.findElement(By.id("com.geekybase.appfortraining:id/viewPager"));
+        assertEquals("Two", content.getAttribute("name"));
+    }
 
     @AfterClass
     public static void tearDown() throws Exception {
