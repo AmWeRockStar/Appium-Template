051708a4c15f684a0e0cd88f4faffd57569f587c
WeRockStar <vr_freestyle@outlook.com>
2016-02-28 17:01:44 +0700

    Add .apk file

--- src/main/java/core/BaseSettings.java
+++ src/main/java/core/BaseSettings.java
@@ -4,20 +4,21 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BaseSettings {
 
     private AndroidDriver driver;
 
-    public AndroidDriver getDriver() {
+    public AndroidDriver getDriver() throws MalformedURLException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidTestDevice");
         if (driver == null)
-            try {
-                driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
-            } catch (Exception e) {
-            }
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+
         return driver;
     }
 }
